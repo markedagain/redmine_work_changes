fa7eef9e1ccc8dd69466e5849d8ec980cdd2d552
root <root@projects.realvision.us>
2013-04-04 16:48:37 -0400

    firs change

--- init.rb
+++ init.rb
@@ -5,4 +5,7 @@ Redmine::Plugin.register :redmine_work_changes do
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
+  
+  menu :top_menu, :all_issues, { :controller => 'issues', :action => 'index', :project_id => @project, :set_filter => 1}, :caption => :label_issue_view_all
+  menu :top_menu, :all_acitivty, { :controller => 'activities', :action => 'index', :project_id => @project, :set_filter => 1}, :caption => :label_overall_activity
 end
